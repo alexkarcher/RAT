1d2c93251756ab110a39e09f9e9662f7548cbadb
Alex Karcher <me@alexkarcher.com>
2016-03-20 21:55:13 -0400

    Updated GitIgnore to ignore Visual Studio debug files

--- tutorial_main_component/JuceLibraryCode/modules/juce_events/juce_events.h
+++ tutorial_main_component/JuceLibraryCode/modules/juce_events/juce_events.h
@@ -1,13 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#if defined (JUCER_XCODE_MAC_F6D2F4CF)
- #include "../../../../../../JUCE/modules/juce_events/juce_events.h"
-#elif defined (JUCER_LINUX_MAKE_6D53C8B4)
- #include "../../../../../../JUCE/modules/juce_events/juce_events.h"
-#elif defined (JUCER_VS2015_78A5022)
- #include "../../../../JUCE-WINDOWS/modules/juce_events/juce_events.h"
-#else
- #error "This file is designed to be used in an Introjucer-generated project!"
-#endif
+#include "../../../../../JUCE/modules/juce_events/juce_events.h"
 
